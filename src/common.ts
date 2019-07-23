25d7cb54d6fb8c6f703503ba94d0b865e5b273c1
wangyang <wangy40@mingyuanyun.com>
2019-07-23 14:27:10 +0800

    补充readme

--- src/common.ts
+++ src/common.ts
@@ -3,7 +3,7 @@ import {ServiceResponse} from './types';
  * 打印错误日志并返回service错误，不会被错误处理中间件捕获
  * @param service 服务名称
  * @param err catch返回的错误
- * @return {Object} {code,error}
+ * @return {ServiceResponse} {code,error}
  */
 export function formatServiceWarning (data: any) {
     let service = data.service || 'unKownService';
@@ -46,12 +46,12 @@ export function formatServiceWarning (data: any) {
 
 /**
  * 聚合各服务返回值在同一个对象内
- * exp { seller: {code: 3, error: ['xxxxxx']}}, project: {code: 0, data: {a:2}} }
- * 聚合成 {code:1, data: {project:{a:2}}, error: ['xxxxxx']}
+ * @example 
+ * // returns {code:1, data: {project:{a:2}}, error: ['xxxxxx']}
+ * formatControllerWarning({ seller: {code: 3, error: ['xxxxxx']}}, project: {code: 0, data: {a:2}} })
  * @date 2019-02-21
- * @export
  * @param {*} params
- * @returns 
+ * @returns {ServiceResponse} {code,error}
  */
 export function formatControllerWarning (params: any): ServiceResponse {
   let res:ServiceResponse = {
